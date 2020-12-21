8fbe4a9926082829be073ac0c88e55018d241458
Philippe Belias <ossimfrancois@yahoo.fr>
2020-12-21 08:04:38 -0500

    configuration of the cmake and first program in opencl done
    Signed-off-by: Philippe Belias <ossimfrancois@yahoo.fr>

---
+++ Opencl/CMakeFiles/sample.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tmp/ParallelProgramming-/Opencl/source/main.c" "/home/tmp/ParallelProgramming-/Opencl/CMakeFiles/sample.dir/source/main.c.o"
+  "/home/tmp/ParallelProgramming-/Opencl/source/opencl.c" "/home/tmp/ParallelProgramming-/Opencl/CMakeFiles/sample.dir/source/opencl.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CL_TARGET_OPENCL_VERSION=220"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Opencl/source/main.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include "opencl.h"
+#include "log.h"
+
+
+#define DATA_SIZE 10
+
+void checkError(cl_int err, char* msg)
+{
+    if (err != CL_SUCCESS)
+    {
+        //LOG_ERROR(msg);
+        printf("%s \n",msg);
+    }
+}
+
+const char* KernelSource = "__kernel void hello(__global float* input, __global float* output) \n"\
+"{\n"\
+"   size_t id = get_global_id(0); \n"\
+"   output[id] = input[id] * input[id]; \n"\
+"}\n"\
+"\n";
+
+int main(){    
+    cl_platform_id platform_id;
+    cl_uint num_platform;
+    cl_device_id device_id;
+    cl_uint num_of_devices;
+    cl_context context;
+    cl_context_properties properties[3];
+    cl_command_queue command_queue;
+    cl_program program;
+    cl_kernel kernel;
+    cl_mem input, output;
+    size_t global;
+
+
+    float inputData[DATA_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    float result[DATA_SIZE] = {0};
+
+    cl_int err = clGetPlatformIDs(1, &platform_id, &num_platform);
+    checkError(err, "No platform found");
+
+    err = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &num_of_devices);
+    checkError(err, "No device found");
+
+    // the properties list must finish with 0
+    properties[0] = CL_CONTEXT_PLATFORM;
+    properties[1] = (cl_context_properties) platform_id;
+    properties[2] = 0;
+
+    // create a context with the gpu device
+    context = clCreateContext(properties, 1, &device_id, NULL, NULL, &err);
+    checkError(err, "error with the context creation");
+
+    // create a command queue using the context and the device
+    command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &err);
+    checkError(err, "error with creation of the command queue");
+
+    // create a program from the kernel source
+    program = clCreateProgramWithSource(context, 1, (const char**) &KernelSource, NULL, &err);
+    checkError(err, "error with the program creation");
+
+    // building the program
+    /*err= clBuildProgram(program, 0,NULL, NULL, NULL, NULL);
+    if (err != CL_SUCCESS)
+    {
+        LOG_ERROR("error with building program";
+        return -1;
+    }*/
+
+
+    if (clBuildProgram(program, 0, NULL, NULL, NULL, NULL) != CL_SUCCESS)
+    {
+        printf("Error building program\n");
+        char buffer[4096];
+        size_t length;
+        clGetProgramBuildInfo(
+            program, // valid program object
+            device_id, // valid device_id that executable was built
+            CL_PROGRAM_BUILD_LOG, // indicate to retrieve build log
+            sizeof(buffer), // size of the buffer to write log to
+            buffer, // the actual buffer to write log to
+            &length
+        ); // the actual size in bytes of data copied to buffer
+        printf("%s\n",buffer);
+        exit(1);
+    }
+
+
+    // kernel object
+    kernel = clCreateKernel(program, "hello", &err);
+    checkError(err, "error with kernel creation");
+
+
+    // create buffers for input and ouput
+    input = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float)* DATA_SIZE, NULL, NULL);
+    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)* DATA_SIZE, NULL, NULL);
+
+    // load data into input buffer
+    clEnqueueWriteBuffer(command_queue, input, CL_TRUE, 0, sizeof(float) * DATA_SIZE, inputData, 0, NULL, NULL);
+
+    // specify the arguments associated with the function
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
+    checkError(err, "association failed for the first argument");
+    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
+    checkError(err, "association failed for the second argument");
+    global = DATA_SIZE;
+
+    // enqueue the kernel command for execution 
+    err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global, NULL, 0, NULL, NULL);
+    checkError(err, "enqueue NDrange failed");
+    clFinish(command_queue);
+
+
+    //copy the result from out of the output buffer
+    clEnqueueReadBuffer(command_queue, output, CL_TRUE, 0, sizeof(float) * DATA_SIZE, result, 0, NULL, NULL);
+    checkError(err, "enqueue read buffer failed");
+
+    //print the results
+    printf("output: ");
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
+        printf("%f \n", result[i]);
+    }
+
+    // clean up 
+    clReleaseMemObject(input);
+    clReleaseMemObject(output);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(command_queue);
+    clReleaseContext(context);
+
+    return 0;
+
+}
