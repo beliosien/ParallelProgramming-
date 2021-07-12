23d0f789dbb1d66d807403d51527d1a59c71d044
philippe <francois-philippe.ossim-belias@polymtl.ca>
2021-07-12 02:57:31 -0400

    animation in opengl done
    Signed-off-by: philippe <francois-philippe.ossim-belias@polymtl.ca>

--- computerGraphics/source/main.cpp
+++ computerGraphics/source/main.cpp
@@ -2,19 +2,23 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+float x_position = -10.0;
+int state = 1;
+
 void display()
 {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
 
-    glBegin(GL_QUADS);
-    glVertex2f(0.0, 5.0);
-    glVertex2f(-4.0, -3.0);
-    glVertex2f(4.0, -3.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(x_position, 1.0);
+    glVertex2f(x_position, -1.0);
+    glVertex2f(x_position + 2.0, -1.0);
+    glVertex2f(x_position + 2.0, 1.0);
     glEnd();
 
-    glFlush();
+    glutSwapBuffers();
 }
 
 void reshape(int w, int h)
@@ -27,10 +31,37 @@ void reshape(int w, int h)
 
 }
 
+void timer(int)
+{
+    glutPostRedisplay();
+    glutTimerFunc(1000/60, timer, 0);
+
+    switch(state)
+    {
+        case 1: 
+            if (x_position < 8)
+            {
+                x_position += 0.15;
+            } else {
+                state = -1;
+            }
+            break;
+
+        case -1:
+            if (x_position > -10)
+            {
+                x_position += -0.15;
+            } else {
+                state = 1;
+            }
+            break;
+    }
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
     glutInitWindowPosition(200, 100);
     glutInitWindowSize(500, 500);
@@ -38,6 +69,7 @@ int main(int argc, char** argv)
     glutCreateWindow("hello");
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutTimerFunc(1000, timer, 0);
 
     glutMainLoop();
 
